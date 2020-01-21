9cafd01c00cac1da26e8b969293474a651b3c0f4
Vladimir Kozhukalov <kozhukalov@gmail.com>
2020-01-21 22:42:48 +0300

    ENTRYPOINT_HOST -> ENDPOINT_HOST

--- idp/keystone/entrypoint.sh
+++ idp/keystone/entrypoint.sh
@@ -6,7 +6,7 @@ fi
 
 export TRANSPORT_URL=${TRANSPORT_URL:-rabbit://keystone:keystone@rabbitmq:5672/openstack/}
 export CONNECTION=${CONNECTION:-mysql+pymysql://keystone:keystone@mysql:3306/keystone}
-export ENTRYPOINT_HOST=${ENTRYPOINT_HOST:-keystone}
+export ENDPOINT_HOST=${ENDPOINT_HOST:-keystone}
 
 envsubst '$TRANSPORT_URL $CONNECTION' < /etc/keystone/keystone.conf.template > /etc/keystone/keystone.conf
 
@@ -15,9 +15,9 @@ keystone-manage fernet_setup --keystone-user root --keystone-group root
 
 keystone-manage bootstrap \
   --bootstrap-password secret \
-  --bootstrap-admin-url http://${ENTRYPOINT_HOST}:5000/v3/ \
-  --bootstrap-internal-url http://${ENTRYPOINT_HOST}:5000/v3/ \
-  --bootstrap-public-url http://${ENTRYPOINT_HOST}:5000/v3/ \
+  --bootstrap-admin-url http://${ENDPOINT_HOST}:5000/v3/ \
+  --bootstrap-internal-url http://${ENDPOINT_HOST}:5000/v3/ \
+  --bootstrap-public-url http://${ENDPOINT_HOST}:5000/v3/ \
   --bootstrap-region-id RegionOne
 
 uwsgi /keystone/httpd/keystone-uwsgi-public.ini
