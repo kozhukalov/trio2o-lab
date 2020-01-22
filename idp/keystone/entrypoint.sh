dee5200209e04e9668a28edc079fcb446c129f35
Vladimir Kozhukalov <kozhukalov@gmail.com>
2020-01-22 19:44:02 +0300

    Keystone as IDP

--- idp/keystone/entrypoint.sh
+++ idp/keystone/entrypoint.sh
@@ -7,11 +7,14 @@ fi
 export TRANSPORT_URL=${TRANSPORT_URL:-rabbit://keystone:keystone@rabbitmq:5672/openstack/}
 export CONNECTION=${CONNECTION:-mysql+pymysql://keystone:keystone@mysql:3306/keystone}
 export ENDPOINT_HOST=${ENDPOINT_HOST:-keystone}
+export IDP_ENTITY_ID=${IDP_ENTITY_ID:-https://${ENDPOINT_HOST}/v3/OS-FEDERATION/saml2/idp}
+export IDP_SSO_ENDPOINT=${IDP_SSO_ENDPOINT:-https://${ENDPOINT_HOST}/v3/OS-FEDERATION/saml2/sso}
 
-envsubst '$TRANSPORT_URL $CONNECTION' < /etc/keystone/keystone.conf.template > /etc/keystone/keystone.conf
+envsubst '$TRANSPORT_URL $CONNECTION $IDP_ENTITY_ID $IDP_SSO_ENDPOINT' < /etc/keystone/keystone.conf.template > /etc/keystone/keystone.conf
 
 keystone-manage --config-file /etc/keystone/keystone.conf db_sync
 keystone-manage fernet_setup --keystone-user root --keystone-group root
+keystone-manage saml_idp_metadata > /etc/keystone/saml2_idp_metadata.xml
 
 keystone-manage bootstrap \
   --bootstrap-password secret \
